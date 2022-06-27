2a53b39fae89a8aee1e29d0ce4b8913139ed6396
sushruth1518 <51015160+sushruth1518@users.noreply.github.com>
2022-06-27 15:30:03 +0530

    Create validate.sh

---
+++ scripts/validate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+aws ecr describe-repositories --repository-names $1 2>&1 > /dev/null
+status=$?
+if [[ ! "${status}" -eq 0 ]]; then
+   # aws ecr create-repository --repository-name $1
+   echo "Repository not exist"
+else 
+    echo "repository $1 already exists "
+    exit 0
+fi
